10f6b9601cbab71ccbae81b542a2f83041d0b3aa
bereketsewnet <121679782+bereketsewnet@users.noreply.github.com>
2024-05-27 14:54:58 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ var server = http.createServer(app);
 const Room = require("./models/room.js");
 // middle ware
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('it works!!');
+});
 
 var io = require("socket.io")(server);
 io.on("connection", (socket) => {
